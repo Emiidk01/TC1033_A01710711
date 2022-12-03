6a703add5350218fcefe2949b29debac61f22d1c
Emiliano <111305406+Emiidk01@users.noreply.github.com>
2022-12-02 20:34:48 -0600

    UPDATE

--- main.cpp
+++ main.cpp
@@ -56,6 +56,9 @@ int main() {
 
 
     do {
+
+        // Muro Principal
+
         system("cls");
         cout<<"\t\t\t**********************************************************"<<endl;
         cout<<"\t\t\t**********************************************************"<<endl;
@@ -63,9 +66,10 @@ int main() {
         cout<<"\t\t\t**********************************************************"<<endl;
         cout<<"\t\t\t**********************************************************"<<endl;
         cout<<"\t\t\t----------------------------------------------------------"<<endl;
-        cout<<"\t\t\t------------ Creador: Emiliano Gomez Gonzalez ------------"<<endl;
+        cout<<"\t\t\t----------------------------------------------------------"<<endl;
         cout<<"\t\t\t----------------------------------------------------------"<<endl;
 
+        // Login de Usuario 
 
         cout<<"\n\t\t\t-----------------[ LOG IN DE USUARIO ]--------------------"<<endl;
         cout<<"\t\t\t----------------------------------------------------------"<<endl;
@@ -95,11 +99,15 @@ int main() {
 
         do{
             
-            
+            // Menu Princiapl
+        
             system ("cls");
             cout<<"\t\t\tMENU PRINCIPAL"<<endl;
             cout<<"\t\t\t--------------"<<endl;
             cout<<"\n\t\tDigite la opcion que desee hacer"<<endl;
+
+
+            // Opciones Posibles 
 
             cout<<"\n\t\t1) Consultar saldo."<<endl;
             cout<<"\t\t2) Consultar mis datos."<<endl;     
@@ -110,108 +118,131 @@ int main() {
             cout<<"\n\t\tOpcion: ";
             cin>>opcion;
 
-
-            switch (opcion){
-                case 1:    
-                    account1.setSaldo(saldo);
-                    cout<<"\n\t\tSu saldo es de: $"<<account1.getSaldo()<<"\n"<<endl; 
-                    
-                    system("pause");
-                    break;
-                case 2: 
-                    account1.setBeneficiario(titular);
-                    account1.consultarDatos();
-                    
-                    system("pause");
-                    break;
-                case 3: 
-                
-                    cout<<"\n\t\tDigita los datos para hacer un ingreso a tu cuenta\n"<<endl;
-                    cout<<"\t\tMonto: $"; cin>>monto;
-                    income1.setMonto(monto);
-                    cout<<"\t\tConcepto: "; cin>>concepto;
-                    income1.setConcepto(concepto);
-                    cout<<"\t\tFecha (dd/mm/aa): "; cin>>fecha;
-                    income1.setFecha(fecha);
-                    cout<<"\t\tProcedencia (Ej: Trabajo): "; cin>>procedencia;
-                    income1.setProcedencia(procedencia);
-
-                    saldo = saldo + monto;
-                    cout<<"\n\t\tSu saldo ha cambiado a: $"<<saldo<<" pesos"<<endl; //Esto es parte del cambio temporal (saldo)
-                    income1.mostrarIngreso();
-                    
-                    
-                    
-                    system("pause");
-                    break;
-                case 4:
-                    
-                    cout<<"\n\t\tDigita los datos para hacer un egreso a tu cuenta\n"<<endl;
-                    cout<<"\t\tMonto: $"; cin>>monto;
-                    discharge1.setMonto(monto);
-                    cout<<"\t\tConcepto: "; cin>>concepto;
-                    discharge1.setConcepto(concepto);
-                    cout<<"\t\tFecha (dd/mm/aa): "; cin>>fecha;
-                    discharge1.setFecha(fecha);
-                    cout<<"\t\tTipo de Egreso (Ej: retiro de efectivo): "; cin>>tipoEgreso;
-                    discharge1.setTipoEgreso(tipoEgreso);
             
-                    if (saldo >= monto){
-                        saldo = saldo - monto;
-                        cout<<"\n\t\tSu saldo ha cambiado a: $"<<saldo<<" pesos"<<endl; //Esto es parte del cambio temporal (saldo)
-                        discharge1.mostrarEgreso();
-                    }
-                    else {
-                        cout<<"\n\t\tNo tienes esa cantidad."<<endl;
-                    }
-                    system("pause");
-                    break;
-                case 5:
-                    
-                    cout<<"\n\t\tDigita los datos para hacer la transferencia\n"<<endl;
-                    cout<<"\t\tMonto: $"; cin>>monto;
-                    transfer1.setMonto(monto);
-                    cout<<"\t\tConcepto: "; cin>>concepto;
-                    transfer1.setConcepto(concepto);
-                    cout<<"\t\tFecha (dd/mm/aa): "; cin>>fecha;
-                    transfer1.setFecha(fecha);
-                    cout<<"\t\tTipo de Transferencia: (Ej: Personal): "; cin>>tipoEgreso;
-                    transfer1.setTipoEgreso(tipoEgreso);
-                    cout<<"\t\tCuenta Destino: "; cin>>cuentaDest;
-                    transfer1.setCuentaDestino(cuentaDest);
-                    cout<<"\t\tBeneficiario Destino: "; cin>>benefDest;
-                    transfer1.setBenefDestino(benefDest);
-                    if (saldo >= monto){
-                        discharge1.mostrarTransferencia();
-                        saldo = saldo - monto;
-                        cout<<"\n\t\tSu saldo ha cambiado a: $"<<saldo<<" pesos"<<endl; 
-                    }
-                    else {
-                        cout<<"\n\t\tLa Transferencia no puede ejecutarse porque no tienes esa cantidad."<<endl;
-                    }
-                    system("pause");
-                    break;
-                case 6:
-                    system("cls");
+                // Condicion Switch Para Elegir
 
-                    account1.setBeneficiario(titular);
-                    account1.salir();
-                    break;
-                default:
-                    cout<<"\n\n\t\tUpps! Parece que pusiste un numero que no viene en la lista. Intenta de nuevo. "<<endl;
-                    system("pause");
+        switch (opcion){
+            case 1:    
+
+                // Caso: Consultar Saldo
+
+                account1.setSaldo(saldo);
+                cout<<"\n\t\tSu saldo es de: $"<<account1.getSaldo()<<"\n"<<endl; 
+                
+                system("pause");
+                break;
+            case 2: 
+
+                //Caso: Consultar Datos
+
+                account1.setBeneficiario(titular);
+                account1.consultarDatos();
+                
+                system("pause");
+                break;
+            case 3: 
+
+                // Caso: Crear un Ingreso
+            
+                cout<<"\n\t\tDigita los datos para hacer un ingreso a tu cuenta\n"<<endl;
+                cout<<"\t\tMonto: $"; cin>>monto;
+                income1.setMonto(monto);
+                cout<<"\t\tConcepto: "; cin>>concepto;
+                income1.setConcepto(concepto);
+                cout<<"\t\tFecha (dd/mm/aa): "; cin>>fecha;
+                income1.setFecha(fecha);
+                cout<<"\t\tProcedencia (Ej: Trabajo): "; cin>>procedencia;
+                income1.setProcedencia(procedencia);
+
+                saldo = saldo + monto;
+                cout<<"\n\t\tSu saldo ha cambiado a: $"<<saldo<<" pesos"<<endl; 
+                income1.mostrarIngreso();
+                
+                
+                
+                system("pause");
+                break;
+            case 4:
+                
+                // Caso: Crear un Egreso
+
+                cout<<"\n\t\tDigita los datos para hacer un egreso a tu cuenta\n"<<endl;
+                cout<<"\t\tMonto: $"; cin>>monto;
+                discharge1.setMonto(monto);
+                cout<<"\t\tConcepto: "; cin>>concepto;
+                discharge1.setConcepto(concepto);
+                cout<<"\t\tFecha (dd/mm/aa): "; cin>>fecha;
+                discharge1.setFecha(fecha);
+                cout<<"\t\tTipo de Egreso (Ej: retiro de efectivo): "; cin>>tipoEgreso;
+                discharge1.setTipoEgreso(tipoEgreso);
+        
+                //Estructura if para evitar egresos con dinero que no se tenga.
+
+                if (saldo >= monto){
+                saldo = saldo - monto;
+                cout<<"\n\t\tSu saldo ha cambiado a: $"<<saldo<<" pesos"<<endl; 
+                discharge1.mostrarEgreso();
+                }
+                else {
+                    cout<<"\n\t\tNo tienes esa cantidad."<<endl;
+                }
+                system("pause");
+                break;
+            case 5:
+
+                //Caso: Hacer una tranferencia
+                
+                cout<<"\n\t\tDigita los datos para hacer la transferencia\n"<<endl;
+                cout<<"\t\tMonto: $"; cin>>monto;
+                transfer1.setMonto(monto);
+                cout<<"\t\tConcepto: "; cin>>concepto;
+                transfer1.setConcepto(concepto);
+                cout<<"\t\tFecha (dd/mm/aa): "; cin>>fecha;
+                transfer1.setFecha(fecha);
+                cout<<"\t\tTipo de Transferencia: (Ej: Personal): "; cin>>tipoEgreso;
+                transfer1.setTipoEgreso(tipoEgreso);
+                cout<<"\t\tCuenta Destino: "; cin>>cuentaDest;
+                transfer1.setCuentaDestino(cuentaDest);
+                cout<<"\t\tBeneficiario Destino: "; cin>>benefDest;
+                transfer1.setBenefDestino(benefDest);
+
+                //Estructura if para evitar tranferencias con dinero que no se tenga.
+
+                if (saldo >= monto){
+                discharge1.mostrarTransferencia();
+                saldo = saldo - monto;
+                cout<<"\n\t\tSu saldo ha cambiado a: $"<<saldo<<" pesos"<<endl; 
+                }
+                else {
+                    cout<<"\n\t\tLa Transferencia no puede ejecutarse porque no tienes esa cantidad."<<endl;
+                }
+                system("pause");
+                break;
+            case 6:
+                system("cls");
+
+                // Caso: Salir de la Cuenta
+
+                account1.setBeneficiario(titular);
+                account1.salir();
+                break;
+            default:
+
+                //En caso de que el usuario ponga un numero que no se tenga 
+
+                cout<<"\n\n\t\tUpps! Parece que pusiste un numero que no viene en la lista. Intenta de nuevo. "<<endl;
+                system("pause");
             }
         
-        
 
-        }while (opcion != 6);
+    }while (opcion != 6); //Condicion del Do While 
 
 
         
     }
     
     
-        
+
     system("pause");
     return 0;
 }
